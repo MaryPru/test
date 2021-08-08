72cf80a1e8d01d66b0877d0c381bfcba87716d9b
MaryPru <prudyusmarina@gmail.com>
2021-08-08 22:43:44 +0300

    [Services] mobile

---
+++ src/pages/ServicesPage/ServicesPage/StrategyItem/Strategy/Strategy.tsx
@@ -0,0 +1,19 @@
+import "./Strategy.css"
+
+interface StrategyProps {
+    title: string
+    description: string
+}
+
+const Strategy = (props: StrategyProps) => {
+    const {title, description} = props
+    return (
+        <div className="strategy">
+
+                <div className="strategyTitle">{title}</div>
+                <div className="strategyDescription"> {description}  </div>
+
+        </div>
+    )
+}
+export default Strategy;
---
+++ src/pages/ServicesPage/ServicesPage/StrategyItem/StrategyItem.tsx
@@ -0,0 +1,29 @@
+import Strategy from "./Strategy/Strategy";
+import "./StrategyItem.css"
+import {useSelector} from "react-redux";
+
+interface StrategyProps {
+    title: string
+    description: string
+}
+
+
+const StrategyItem = () => {
+
+    const StrategyData = useSelector((state: any) => state.servicesReducers)
+
+    return (
+    <div className="strategyItem">
+        <div className={"contentWrapper strategyWrapper"} >
+            {StrategyData.StrategyInfo.map((strategyItem:StrategyProps)=>(
+               <Strategy
+               title={strategyItem.title}
+               description={strategyItem.description}
+               />
+                )
+            )}
+        </div>
+    </div>
+    )
+}
+export default StrategyItem;
